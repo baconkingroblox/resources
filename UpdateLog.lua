e8ec760719591523cac72c498afe6940f4572934
baconkingroblox <114178329+baconkingroblox@users.noreply.github.com>
2022-09-22 19:16:35 +0200

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,6 +1,6 @@
-local updates = 
+updates = 
 {
 [1] = {["Update"] = "Test1", ["Color"] = Color3.fromRGB(0,255,0)};
 [2] = {["Update"] = "Test2", ["Color"] = Color3.fromRGB(255,255,0)};
 }
-local dateu = "September 22, 2022"
+dateu = "September 22, 2022"
