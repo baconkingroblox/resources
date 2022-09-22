57b84835c0ed992c20e17b90148b850e02614611
baconkingroblox <114178329+baconkingroblox@users.noreply.github.com>
2022-09-22 19:35:39 +0200

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,6 +1,6 @@
 {
-  ["Date"] = "September 22, 2022";
-[1] = {["Update"] = "Test1", ["Color"] = Color3.fromRGB(0,255,0)};
-[2] = {["Update"] = "Test2", ["Color"] = Color3.fromRGB(255,255,0)};
+  "Date":"September 22, 2022";
+"1":{["Update"]:"Test1","Color":Color3.fromRGB(0,255,0)};
+"2":{"Update":"Test2","Color":Color3.fromRGB(255,255,0)};
 }
 
